cd5f9c9997e1493f01a5cac2f6e65b4e484d50fc
Ojasvi Kumar <ojasvikumar007@gmail.com>
2024-11-08 17:37:35 +0530

    Leetcode Day 281

--- DecisionTreeC45.py
+++ DecisionTreeC45.py
@@ -24,3 +24,5 @@ accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy:", accuracy)
 print("Classification Report:\n", classification_report(y_test, y_pred))
 print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
+
+
