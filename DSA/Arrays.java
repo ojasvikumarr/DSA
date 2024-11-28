e6ee8ddb0aa83b1964e78730e0b0a68850801cfc
Ojasvi Kumar <ojasvikumar007@gmail.com>
2024-11-28 11:05:59 +0530

    Hard

--- DSA/Arrays.java
+++ DSA/Arrays.java
@@ -34,7 +34,7 @@ public class Arrays {
 
 
     }
-    public static void fill(char[] row, int i) {
+    public static void fill(int[] row, int i) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'fill'");
     }
