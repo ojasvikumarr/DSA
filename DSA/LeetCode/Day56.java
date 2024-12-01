41c92100dd75fa56f4449aab80630f182a1f6dde
Ojasvi Kumar <ojasvikumar007@gmail.com>
2024-12-01 12:17:56 +0530

    Toughest ToC

---
+++ DSA/LeetCode/Day56.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+public class Day56 {
+
+    public boolean checkIfExist(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : arr) {
+            if (set.contains(num * 2) || (num % 2 == 0 && set.contains(num / 2))) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
+
