d51e7ff32f5dc9610d7c9932944996a6a05e6d92
Ojasvi Kumar <ojasvikumar007@gmail.com>
2024-12-23 12:53:12 +0530

    is A Subsequence

---
+++ DSA/DynamicProgramming/isSubsequencee.java
@@ -0,0 +1,19 @@
+package DynamicProgramming;
+
+public class isSubsequencee {
+    class Solution {
+        public boolean isSubsequence(String s, String t) {
+            int i = 0 ; 
+            int j = 0 ; 
+            while( i < s.length() && j < t.length() ){
+                if(s.charAt(i) == t.charAt(j)){
+                    i++ ; 
+                    j++ ; 
+                }else{
+                    j++ ;
+                }
+            }
+            return i == s.length() ;
+        }
+    }
+}
