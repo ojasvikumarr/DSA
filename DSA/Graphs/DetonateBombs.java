6343f367f733688ebc396290b2ad5e50a61d0433
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-01-16 19:23:31 +0530

    Detonation

--- DSA/Graphs/DetonateBombs.java
+++ DSA/Graphs/DetonateBombs.java
@@ -1,70 +1,56 @@
 package Graphs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DetonateBombs {
-    class Solution {
-        int[] parent ; 
-        int[] rank ; 
-        public void init(int n){
-            rank = new int[n];
-            parent = new int[n];
-            for(int i = 0 ; i < n ; i++) parent[i] = i ;
-        }
-        public int find(int x){
-            if(parent[x] == x){
-                return x ;
-            }
-            return parent[x] = find(parent[x]);
-        }
-        public void union(int u , int v){
-            int parU = find(u);
-            int parV = find(v);
-            if(parU != parV){
-                if(rank[parU] == rank[parV]){
-                    parent[parU] = parV;
-                    rank[parV]++ ;
-                }else if(rank[parU] > rank[parV]){
-                    parent[parV] = parU ;
-                }else{
-                    parent[parU] = parV ;
+    
+class Solution {
+    public int maximumDetonation(int[][] bombs) {
+        //Approach will be to make a graph 
+        //Reachibility will be checked by
+        //(x-x1)^2 + (y-y1)^2 > r^2 this means point is outside 
+        //(x-x1)^2 + (y-y1)^2 <= r^2 this means point is inside
+        int n = bombs.length;
+        List<List<Integer>> adjList = new ArrayList<>();
+
+        // Build adjacency list
+        for (int i = 0; i < n; i++) {
+            adjList.add(new ArrayList<>());
+            for (int j = 0; j < n; j++) {
+                if (i != j && checkConnectivity(bombs[i], bombs[j])) {
+                    adjList.get(i).add(j);
                 }
             }
         }
-        public int maximumDetonation(int[][] bombs) {
-            //Approach will be to make a graph 
-            //Reachibility will be checked by
-            //(x-x1)^2 + (y-y1)^2 > r^2 this means point is outside 
-            //(x-x1)^2 + (y-y1)^2 <= r^2 this means point is inside
-            //We will connec the nodes using dsu 
-            //The component with the largest nodes will be the anwer 
-            init(bombs.length);
-            for(int i = 0 ; i < bombs.length ; i++){
-                int[] mainbomb = bombs[i];
-                for(int j = 0 ; j < bombs.length ; j++){
-                    int[] secbomb = bombs[j];
-                    int x = mainbomb[0];
-                    int y = mainbomb[1];
-                    int r = mainbomb[2];
-                    int x1 = secbomb[0];
-                    int y1 = secbomb[1];
-                    if( i!=j && checkConnectivity(x , y , x1 , y1, r)){
-                        union(i , j);
-                    }
-                }
-            }
-            int[] freq = new int[bombs.length];
-            int maxi = 0 ; 
-            for(int i = 0 ; i < bombs.length ; i++){
-                freq[find(i)]++ ;
-                maxi = Math.max(maxi , freq[find(i)]);
-            }
-            return maxi ;
+
+        // Perform DFS for each bomb
+        int maxDetonations = 0;
+        for (int i = 0; i < n; i++) {
+            boolean[] visited = new boolean[n];
+            maxDetonations = Math.max(maxDetonations, dfs(i, adjList, visited));
         }
-        public boolean checkConnectivity(int x , int y , int x1 , int y1 , int r){
-            long dx = x-x1 ;
-            long dy = y-y1 ;
-            long dist = dx*dx + dy*dy ; 
-            long radius = (long) r*r ; 
-            return dist <= radius ;
-        }
+
+        return maxDetonations;
     }
+
+    private int dfs(int node, List<List<Integer>> adjList, boolean[] visited) {
+        visited[node] = true;
+        int count = 1; // Count the current bomb
+        for (int neighbor : adjList.get(node)) {
+            if (!visited[neighbor]) {
+                count += dfs(neighbor, adjList, visited);
+            }
+        }
+        return count;
+    }
+
+    private boolean checkConnectivity(int[] bomb1, int[] bomb2) {
+        long dx = bomb1[0] - bomb2[0];
+        long dy = bomb1[1] - bomb2[1];
+        long distSq = dx * dx + dy * dy;
+        long radiusSq = (long) bomb1[2] * bomb1[2];
+        return distSq <= radiusSq;
+    }
+}
 }
