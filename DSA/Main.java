50e0fa44789204a5a9b602384f3bf70066a4623a
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-01-26 19:07:56 +0530

    AtCoders Div2

---
+++ DSA/Main.java
@@ -0,0 +1,72 @@
+// package AtCoders;
+//Remove this package when submitting the answer 
+import java.util.* ;
+import java.lang.StringBuilder;
+// import java.math.BigInteger; 
+public class Main {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        String s = sc.next() ;
+        String t = sc.next();
+        // System.out.println();
+        solve(s, t, n, m);
+        sc.close();
+    }
+    public static void solve(String s, String t , int n , int m){
+        //We will sort the frequencies 
+        //Or we can just make a freq map 
+        int[] freq = new int[10];//1-9
+        
+        //We need to keep track of their index 
+        //We will keep the max index in map 
+        Map<Integer , Deque<Integer>> map = new HashMap<>();
+        for(int i = 0 ; i < 10 ; i++){
+            map.put(i , new ArrayDeque<>());
+        }
+        for(int i = 0 ; i < m ; i++){
+            char c = t.charAt(i);
+            int num = c-'0';
+            freq[num]++ ;
+            map.get(num ).add(i);
+        }
+        //Now we will traverse String s 
+        StringBuilder sb = new StringBuilder(s);
+
+        int maxi = -1 ; 
+        int i = 0 ;
+        for(int j = 9 ; j > 0 ; j--){
+            while(freq[j] > 0){
+                int num = j ; 
+                boolean placed = false ;
+                // System.out.println(j + " " + freq[j]);
+                while(i < n){
+                    if(sb.charAt(i) - '0' < num){
+                        placed = true ;
+                        sb.setCharAt(i , (char)(num+'0')) ; 
+                        // System.out.println("Ping!!" + sb);
+                        if(map.containsKey(num))
+                        maxi = Math.max(maxi , map.get(num).pollLast());
+                        break ;
+                    }
+                    i++ ;
+                }
+                if(placed == false){
+                    //that is the num is not placed we need to check maxi 
+                    int index = -1 ;
+                    if(map.containsKey(num)) index = map.get(num).pollFirst() ;
+                    if(index !=-1 && index > maxi){
+                        //we will place at last 
+                        sb.setCharAt(sb.length()-1 , (char)(num+'0')) ;
+                        maxi = index;
+                    }
+                }
+                freq[j]-- ;
+            }
+        }
+        // BigInteger result = new BigInteger(sb.toString());
+        System.out.println(sb);
+        return ;
+    }
+}
