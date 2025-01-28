2d72f73fc609d5d8287bd3df58d9c01809ccfa7c
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-01-28 11:57:36 +0530

    GCD GP checkinggit add .

--- DSA/AtCoders/Main.java
+++ DSA/AtCoders/Main.java
@@ -5,11 +5,13 @@ import java.util.* ;
 public class Main {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        int n = 5 ;
+        int n = sc.nextInt() ;
         int[] arr = new int[n];
-        for(int i =0 ; i < n ; i++) arr[i] = sc.nextInt();
-
-        if(solve(arr)){
+        
+        for(int i = 0 ; i < n ; i++){ 
+            arr[i] = sc.nextInt();
+        }
+        if(solveII(arr , n)){
             System.out.println("Yes");
         }else{
             System.out.println("No");
@@ -18,7 +20,31 @@ public class Main {
         sc.close();
     }
 
-    
+    public static boolean solveII(int[] arr , int n){
+        if(n <= 2) return true ; 
+
+        // double ratio = (double)(arr[1]/arr[0]);
+        int Gcd = gcd(arr[1] , arr[0]);
+
+        String ratio = String.valueOf(arr[1]/Gcd + "-" + arr[0]/Gcd);
+
+        for(int i = 0 ;i < n-1 ; i++){
+            int GCd = gcd(arr[i+1] , arr[i]);
+            String ratioI = String.valueOf(arr[i+1]/GCd + "-" + arr[i]/GCd);
+            if(!ratio.equals(ratioI)){
+                return false ;
+            }
+        }
+        return true ; 
+    }
+    public static int gcd(int a , int b){
+        while(a!=0){
+            int temp = a ; 
+            a = b % a ;
+            b = temp ; 
+        }
+        return b ; 
+    }
     public static boolean solve(int[] arr){
         int count = 0 ; 
         for(int j = 0 ; j < 4 ; j++){
