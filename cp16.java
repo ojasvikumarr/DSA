ffc6cfbf622182d33e1830036fc2f92899a3ef2e
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-01-30 15:27:31 +0530

    Day 364

--- cp16.java
+++ cp16.java
@@ -1,66 +1,117 @@
-import java.util.* ;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
 
-public class cp16 {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while(t-- >0){
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		for(int tc = 0 ; tc < t ; tc++){
             int n = sc.nextInt();
-            int[] a = new int[n];
-            int[] b = new int[n];
-            for(int i =0 ; i < n ; i++) a[i] = sc.nextInt();
-            for(int i =0 ; i < n ; i++) b[i] = sc.nextInt();
-            solve(a , b);
+            int p = sc.nextInt();
+            int[] arr = new int[n];
+            for(int i =0 ; i < n ; i++) arr[i] = sc.nextInt();
+            solve(arr , n , p);
+		}
+		sc.close();
+	}
+    public static void solveII(int[] arr , int n , int p ){
+        int[] res = new int[n];
+        Arrays.fill(res , Integer.MAX_VALUE);
 
-        }
-        sc.close();
-    }
-    public static void solveII(int[] avail , int[] need){
-        int count = 0 ;
-        for(int i = 0; i < avail.length ; i++){
-            if(avail[i] < need[i]){
-                count++ ;
-            }
-        }
-        if(count > 1){
-            System.out.println("No");
-        }else{
-            System.out.println("Yes");
-        }
-        return ;
-    }
-    public static void solve(int[] available , int[] needed ){
-        int n = needed.length;
-        if(n == 1){
-            if(available[0] < needed[0]){
-                System.out.println("NO");
-                return ;
-            }
-            System.out.println("YES");
-            return ;
-        }
-        int i = 0 ; 
-        while(i < n){
-
-            if(available[i] < needed[i]){
-                if(decrement(available , i)){
-                    System.out.println("NO");
-                    return ;
+        for(int i = 0 ; i < n ; i++){
+            if(arr[i] == 0){
+                //left 
+                if(i-1 >= 0){
+                    int l = i ; 
+                    // res[i-1] = (int)Math.ceil((double)(arr[i-1]/p)) ; 
+                    while(l >=0){
+                        if(arr[l] > arr[l+1]){
+                            res[l] = (int)Math.ceil((double)(arr[l]/p)) ; 
+                        }
+                        l-- ;
+                    }
                 }
-                available[i]++ ;
-                i = 0 ;
+                //right 
+                if(i+1 < n){
+                    int r = i+1 ;
+                    while(r < n){
+                        if(arr[r] < arr[r+1]){
+                            res[r] = (int)Math.ceil((double)(arr[r]/p)) ; 
+                        }
+                        r-- ;
+                    }
+                    res[i+1] = (int)Math.ceil((double)(arr[i+1]/p)) ; 
+                }
             }
-            i++ ;
         }
-        System.out.println("YES");
-        return ;
+        //Now the remaining 
+        for(int i = 0 ; i < n ; i++){
+            if(res[i] == Integer.MAX_VALUE){
+                //check min in the left and right 
+                //left 
+                int l = i-1 ; 
+                while(l >= 0){
+                    if(res[l] < Integer.MAX_VALUE){
+                        res[i] = res[l];
+                        break ;
+                    }
+                    l++; 
+                }
+                int r = i+1 ; 
+                while(r < n){
+                    if(res[r] < Integer.MAX_VALUE){
+                        res[i] = Math.min(res[i] , res[r]);
+                        break ;
+                    }
+                    r++ ;
+                }
+            }
+        }
     }
-    public static boolean decrement(int[] arr , int i){
-        for(int j = 0 ; j <arr.length ; j++){
-            if(j == i) continue ;
-            if(arr[j] == 0) return true ;
-            arr[j]-- ;
+    public static void solve(int[] arr , int n , int p){
+        int[] res = new int[n];
+        Arrays.fill(res , Integer.MAX_VALUE);
+
+        for(int i = 0 ; i < n ; i++){
+            if(arr[i] == 0){
+                //its a pot hole 
+                //We go left and right till we encounter ends or another pot hole 
+                int l = i-1 ; 
+                int htPrev = 0;
+                int r = i+1 ; 
+                while(l >= 0 ){
+                    if(arr[l] == 0) break ; 
+                    if(l == i-1){
+                        htPrev = arr[l];
+                        res[l] = arr[l]/p ; 
+                    }else{
+                        if(arr[l] < htPrev){
+                            res[l] = res[l+1];
+                        }else{
+                            res[l] = arr[l]/p ; 
+                        }
+                    }
+                    l-- ;
+                }
+                while(r < n){
+                    if(arr[r] == 0) break ; 
+                    if(r == i+1){
+                        htPrev = arr[r];
+                        res[r] = arr[r]/p ; 
+                    }else{
+                        if(arr[r] < htPrev){
+                            arr[r] = arr[r-1];
+                        }else{
+                            arr[r] = arr[r]/p ; 
+                        }
+                    }
+                    r++ ;
+                }
+            }
         }
-        return false ;
     }
 }
