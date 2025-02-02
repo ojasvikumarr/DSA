ff501f61262f525a3eeb5030fd95679c46434606
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-02-02 22:47:24 +0530

    Day 366

---
+++ DSA/LeetCode/Day117.java
@@ -0,0 +1,15 @@
+package LeetCode;
+
+public class Day117 {
+    class Solution {
+        public boolean check(int[] nums) {
+            //we should encounter a step down only once 
+            int count = 0 ; 
+            int n = nums.length ;
+            for(int i = 0 ; i < n ; i++){
+                if(nums[i] > nums[(i+1)%n]) count++ ; 
+            }
+            return count <= 1 ? true : false ;
+        }
+    }
+}
