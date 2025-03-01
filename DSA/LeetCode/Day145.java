cce1a96753cac9373d50d906df6e76b408087cc1
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-03-01 08:37:45 +0530

    Day 394

---
+++ DSA/LeetCode/Day145.java
@@ -0,0 +1,20 @@
+package LeetCode;
+
+public class Day145 {
+    class Solution {
+        public int[] applyOperations(int[] nums) {
+            int n = nums.length ; 
+            int[] result = new int[n];
+            int idx = 0 ; 
+            for(int i = 0 ; i < n-1 ; i++){
+                if(nums[i] == nums[i+1]){
+                    nums[i] *= 2; 
+                    nums[i+1] = 0 ; 
+                }
+                if(nums[i] != 0 ) result[idx++] = nums[i];
+            }
+            if(nums[n-1] != 0) result[idx++] = nums[n-1];
+            return result; 
+        }
+    }
+}
