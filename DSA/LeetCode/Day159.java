5832396e1b8eb60d2974886a27684346cf4b6978
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-03-12 10:10:06 +0530

    Day 406

---
+++ DSA/LeetCode/Day159.java
@@ -0,0 +1,13 @@
+public class Day159 {
+    class Solution {
+        public int maximumCount(int[] nums) {
+            int pos = 0 ; 
+            int neg = 0 ; 
+            for(Integer it : nums){
+                if(it > 0) pos++ ; 
+                else if(it < 0) neg++ ;
+            }
+            return Math.max(pos , neg);
+        }
+    }
+}
