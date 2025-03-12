2df7206b03f2c9b40cd0dbd9b955b109f096c1f5
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-03-12 22:59:34 +0530

    CodeChef

--- CodeChef.java
+++ CodeChef.java
@@ -10,90 +10,37 @@ class CodeChef
 		Scanner sc = new Scanner(System.in);
 		int t = sc.nextInt();
 		for(int tc = 0 ; tc < t ; tc++){
-		    int n = sc.nextInt();
-		    int m = sc.nextInt();
-		    int[] a = new int[n];
-		    int[] b = new int[m];
-		    for(int i = 0 ; i < n ; i++) a[i] = sc.nextInt();
-		    for(int j = 0 ; j < m ; j++) b[j] = sc.nextInt();
-		    solve(a , b , n , m);
+            int n = sc.nextInt();
+            List<Integer> x = new ArrayList<>();
+            int[] y = new int[n];
+            for(int i =0 ; i < n ;i++){
+                x.add(sc.nextInt());
+            }
+            for(int i = 0 ; i < n ; i++){
+                y[i] = sc.nextInt() ; 
+            }
+            solve(x, y , n);
 		}
 		sc.close();
 	}
-	public static void solve(int[] a , int[] b , int n , int m){
-	    //First sort the array b 
-	    int mini = Integer.MAX_VALUE ; 
-	    int idx = 0 ; 
-	    for(int i = 0 ; i < m ; i++){
-            // System.out.print(b[i] + " - "); 
-	        if(mini > b[i]){
-	            mini = Math.min(mini , b[i]);
-	            idx = i ; 
-	        }
-	    }
-	    int[] B = new int[m];
-	    for(int i = 0 ; i < m ; i++){
-	        B[i] = b[(idx+i)%m];
-            // System.out.print(B[i] +  " - ");
-	    }
-	    
-	    //Now we have sorted B 
-	   // ??Now we start comparing 
-	   
-	   int h = 0 ; //For a 
-	//    int j = 0 ; //For B 
-    if(m == 1){
-        for(int i = 0 ; i < n; i++){
-            if(a[i] > b[0]){
-                a[i] = b[0];
+    public static void solve(List<Integer> x, int[] y , int n){
+        int pivot = y[0];
+        int index = x.indexOf(pivot) + 1 ; 
+        Collections.sort(x , (a , b) ->{
+            int A = Math.abs(a-pivot);
+            int B = Math.abs(b-pivot);
+            if(A == B){
+                return a - b ; 
+            }
+            return A-B ; 
+        }) ; 
+        for(int i = 0 ; i < n ; i++){
+            if(x.get(i) != y[i]){
+                System.out.println(-1);
+                return ;
             }
         }
-        // System.out.println("HEllo");
-    }else{
-        //If aray b is 2 
-           while(h < n){
-               if(B[0] <= a[h]){
-                   //We lay down B 
-                   int i = h ;
-                   int k = 0 ; 
-                   if(n-i >= m){
-                       boolean flag = false ; 
-                       while(k < m && i < n){
-                           if(B[k] < a[i]){
-                            flag = true ; 
-                            break ; 
-                           }
-                           k++ ; 
-                           i++ ; 
-                       }
-                       i = h ; 
-                       k = 0 ;
-                       if(flag == true){
-                           while(i < n - m){
-                                a[i] = B[0];
-                                i++ ;
-                           }
-                           while(i < n && k < m){
-                                a[i] = B[k];
-                                i++; 
-                                k++ ; 
-                           }
-                           break ; 
-
-                       }
-                    //    while(flag && k < m && i < n){
-                    //        //Dominos effect 
-                           
-                    //    }
-                   }
-               }
-               h++ ; 
-           }
+        System.out.println(index);
+        return ; 
     }
-	   for(Integer it : a){
-	       System.out.print(it + " ");
-	   }
-	   System.out.println();
-	   return ; 
-	}
 }
