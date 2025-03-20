ae8922a5ac6c93595179c4f03f9f6771f01ef413
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-03-20 09:43:02 +0530

    Target SUm

--- CodeChef.java
+++ CodeChef.java
@@ -2,45 +2,35 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class CodeChef
+class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
 		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		for(int tc = 0 ; tc < t ; tc++){
-            int n = sc.nextInt();
-            List<Integer> x = new ArrayList<>();
-            int[] y = new int[n];
-            for(int i =0 ; i < n ;i++){
-                x.add(sc.nextInt());
-            }
-            for(int i = 0 ; i < n ; i++){
-                y[i] = sc.nextInt() ; 
-            }
-            solve(x, y , n);
-		}
+		    int t = sc.nextInt();
+		    while(t-- > 0){
+		        int n = sc.nextInt();
+		        int[] arr = new int[n];
+		        for(int i = 0 ; i < n ; i++) arr[i] = sc.nextInt();
+		        solve(arr , n);
+		    }
 		sc.close();
 	}
-    public static void solve(List<Integer> x, int[] y , int n){
-        int pivot = y[0];
-        int index = x.indexOf(pivot) + 1 ; 
-        Collections.sort(x , (a , b) ->{
-            int A = Math.abs(a-pivot);
-            int B = Math.abs(b-pivot);
-            if(A == B){
-                return a - b ; 
-            }
-            return A-B ; 
-        }) ; 
-        for(int i = 0 ; i < n ; i++){
-            if(x.get(i) != y[i]){
-                System.out.println(-1);
-                return ;
-            }
-        }
-        System.out.println(index);
-        return ; 
-    }
+	public static void solve(int[] arr , int n ){
+	    Map<Integer , Integer> map = new HashMap<>();
+	    int[] prefix = new int[n+1];
+	    for(int i = 0 ; i < n ; i++){
+	        if(map.containsKey(arr[i])){
+	            int idx = map.get(arr[i]) ; 
+	            prefix[i+1] = Math.min(prefix[idx+1] , prefix[i]+ (arr[i-1] == arr[i] ? 0 : 1) ) ;
+                map.put(arr[i] , i);
+	        }else{
+	            map.put(arr[i] , i);
+	            prefix[i+1] = prefix[i] + 1 ;
+	        }
+	    }
+	    System.out.println(prefix[n]);
+	    return ; 
+	}
 }
---
+++ PlacementTime/DP/TargetSum.java
@@ -0,0 +1,63 @@
+package PlacementTime.DP;
+
+import java.util.Arrays;
+
+public class TargetSum {
+    class Solution {
+    static int memo[][] ; 
+    static Boolean isSubsetSum(int arr[], int sum) {
+        // code here
+        //i have two options on every index 
+        //either i pick or i dont pick it 
+        //as there can be many solutios to the problem 
+        //i will be opting for a resursive solution 
+        //exploring all the pahs and return true if theres a path to the sum 
+        int n = arr.length ; 
+        memo = new int[n][sum+1];
+        for(int[] row : memo) Arrays.fill(row , -1);
+        // return solve(arr , n-1 , sum);
+        return tabulation(arr , sum , n);
+    }
+    public static boolean tabulation(int[] arr , int sum , int n ){
+        boolean[][] tab = new boolean[n][sum+1];
+        //bottom up appraoch 
+        //from the base case to the required case 
+        //what will be the base case?
+        // if sum is 0 then we accept it 
+        for(int i = 0 ; i < n ; i++){
+            tab[i][0] = true ; 
+        }
+        if(arr[0] <= sum)
+        tab[0][arr[0]] = true ; 
+        for(int i = 1 ; i < n ; i++){
+            for(int target = 1 ; target <= sum ; target++){
+                boolean pick = false ; 
+                if(target - arr[i] >= 0){
+                    pick = tab[i-1][target-arr[i]];
+                }
+                boolean notpick = tab[i-1][target];
+                tab[i][target] = pick || notpick ;
+            }
+        }
+        return tab[n-1][sum];
+    }
+    public static boolean solve(int[] arr , int idx , int sum){
+        if(sum == 0){
+            return true ; 
+        }else if(idx < 0){
+            return false ; 
+        }
+        if(memo[idx][sum] != -1){
+            return memo[idx][sum] == 1 ? true : false ; 
+        }
+        boolean pick = false ; 
+        if(sum - arr[idx] >= 0){
+            pick = solve(arr , idx-1 , sum - arr[idx]) ;
+        }
+        boolean notpick = solve(arr , idx-1 , sum) ;
+        boolean result = pick || notpick ; 
+        memo[idx][sum] = (result ? 1 : 0);
+        return result ; 
+    }
+}
+}
