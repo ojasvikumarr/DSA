6a49d0f7541e77bf81ac034e70191a14d0747d8b
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-03-20 20:47:22 +0530

    JPMC OA

---
+++ DSA/DynamicProgramming/mergeIntervals.java
@@ -0,0 +1,32 @@
+package DynamicProgramming;
+
+public class mergeIntervals {
+    class Solution {
+        public int countBinarySubstrings(String s) {
+            int i = 0 ; 
+            int n = s.length();
+            int zero = 0 ; 
+            int one = 0 ; 
+            int res = 0 ; 
+            while(i < n){
+                if(s.charAt(i) == '0') zero++ ; 
+                else one++ ; 
+                i++ ; 
+                while(i < n && s.charAt(i) == s.charAt(i-1)){
+                    if(s.charAt(i) == '0') zero++ ; 
+                    else one++ ; 
+                    i++ ;
+                }
+                if(zero != 0 && one != 0){
+                    res += Math.min(zero , one);
+                    if(s.charAt(i-1) == '0'){
+                        one = 0 ; 
+                    }else{
+                        zero = 0 ; 
+                    }
+                }
+            }
+            return res ; 
+        }
+    }
+}
