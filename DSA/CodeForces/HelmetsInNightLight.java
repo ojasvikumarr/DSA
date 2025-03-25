d9682faffcc27b8e8a7cbf13797e4b941eb2153a
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-03-25 10:19:17 +0530

    Helmets in Night Light

--- DSA/CodeForces/HelmetsInNightLight.java
+++ DSA/CodeForces/HelmetsInNightLight.java
@@ -1,6 +1,9 @@
+package CodeForces;
+
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.Arrays ;
-public class cp55 {
+
+public class HelmetsInNightLight {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
