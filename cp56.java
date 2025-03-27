ff33ca3eb17ecf3d75f0b76b71b9a1d46e877463
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-03-27 10:58:25 +0530

    Deja Vu

--- cp56.java
+++ cp56.java
@@ -9,18 +9,50 @@ public class cp56 {
             int q = sc.nextInt();
             int[] arr = new int[n];
             for(int i = 0 ; i < n; i++) arr[i] = sc.nextInt();
+            int[] query = new int[q];
             for(int i = 0 ; i < q ; i++){
-                int x = sc.nextInt();
-                // System.out.print((1<<(x)) + " ");
-                for(int j = 0 ; j < n; j++){
-                    if((arr[j] % (1<<(x))) == 0){
-                        arr[j] += (1<<(x-1)) ;
-                    }
+                query[i] = sc.nextInt();
+            }
+
+            int k = 0 ; 
+            for(int i : query){
+                if(k % (1 << i) == 0){
+                    k |= 1 << (i - 1);
                 }
             }
-            for(int i = 0 ; i < n; i++){
-                System.out.print(arr[i] + " ");
+
+            for(int i = 0 ; i < n ; i++){
+                int j =arr[i]&-arr[i];
+                arr[i] |= k & (j-1);
             }
+
+            for(int num : arr){
+                System.out.print(num + " ");
+            }
+            // int limit = -1; 
+            // long lastbit = Integer.MIN_VALUE ; 
+            // for(int i =0 ; i < n ; i++){
+            //     lastbit = Math.max(lastbit ,((arr[i]&-arr[i])));
+            // }
+
+            // for(int i = 0 ; i < q ; i++){
+            //     int x = query[i];
+            //      // System.out.println(lastbit);
+            //     // System.out.print((1<<(x)) + " ");
+            //     if((x) <= lastbit){
+            //         for(int j = 0 ; j < n; j++){
+            //             if((arr[j] % (1<<(x))) == 0){
+            //                 arr[j] += (1<<(x-1)) ;
+            //                 // limit = x-1 ; 
+            //                 lastbit = ((x-1));
+            //             }
+            //         }
+            //         // System.out.println("Limit" + limit);
+            //     }
+            // }
+            // for(int i = 0 ; i < n; i++){
+            //     System.out.print(arr[i] + " ");
+            // }
             System.out.println();
         }
         sc.close();
