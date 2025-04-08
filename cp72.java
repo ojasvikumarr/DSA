ff745a3b2ccc6cb51f00d0def4f6b2896de45ed6
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-04-08 15:47:34 +0530

    Buying torches

--- cp72.java
+++ cp72.java
@@ -1,4 +1,16 @@
 import java.util.* ; 
 public class cp72 {
-    
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            long x = sc.nextInt();
+            long y = sc.nextInt();
+            long k = sc.nextInt();
+
+            long ans =((y*k+k-1 + x - 2)/(x-1)) + k;
+            System.out.println(ans);
+        }
+        sc.close();
+    }
 }
