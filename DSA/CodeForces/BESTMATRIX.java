8c6c9203c8072cb50d21933a335945d4e205d79a
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-04-10 17:34:29 +0530

    Best MAtrix

--- DSA/CodeForces/BESTMATRIX.java
+++ DSA/CodeForces/BESTMATRIX.java
@@ -1,10 +1,9 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
-class Codechef
-{
-	public static void main (String[] args) throws java.lang.Exception
+public class BESTMATRIX{
+    public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
 		Scanner sc = new Scanner(System.in);
