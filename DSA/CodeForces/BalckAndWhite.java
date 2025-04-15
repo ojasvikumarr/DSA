1ebc1b83ad3f3d40120a6b6f82cb1ae8f58ebf9a
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-04-15 21:51:55 +0530

    Black and White

--- DSA/CodeForces/BalckAndWhite.java
+++ DSA/CodeForces/BalckAndWhite.java
@@ -1,5 +1,8 @@
-import java.util.*; 
-public class cp75 {
+package CodeForces;
+
+import java.util.Scanner;
+
+public class BalckAndWhite {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
