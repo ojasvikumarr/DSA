9f8d7c153fab6f65837fc58f8d2c5bef6360cb9d
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-04-15 18:36:11 +0530

    Day 439

--- cp75.java
+++ cp75.java
@@ -1,4 +1,20 @@
 import java.util.*; 
 public class cp75 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-- > 0){
+            int n = sc.nextInt();
+            int ele = 1<<((int)Math.floor(Math.log(n)/Math.log(2)));
+            for(int i = 1 ; i < n ; i++){
+                if(i == ele){
+                    System.out.print("0 ");
+                }
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
     
 }
