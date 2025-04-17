013063fd1ee61e19b2ce1703c4ea47539a7165e9
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-04-17 11:12:56 +0530

    Day 431

---
+++ DSA/LeetCode/Day194.java
@@ -0,0 +1,18 @@
+package LeetCode;
+
+public class Day194 {
+    class Solution {
+        public int countPairs(int[] nums, int k) {
+            int res = 0 ; 
+            int n = nums.length ;
+            for(int i = 0 ; i < n ; i++){
+                for(int j = i+1 ; j < n ; j++){
+                    if(nums[i] == nums[j] && (i*j) % k == 0){
+                        res++ ;
+                    }
+                }
+            }
+            return res ;
+        }
+    }
+}
