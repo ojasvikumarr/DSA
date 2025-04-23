3ebf3aef426f4641b511e4c006c7275133705356
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-04-23 23:02:45 +0530

    CodeChef Start 183

--- DSA/CodeChef.java
+++ DSA/CodeChef.java
@@ -1,40 +1,33 @@
 import java.util.*;
-import java.lang.*;
+import java.lang.StringBuilder;
 import java.io.*;
 
 class CodeChef
 {
-	public static void main (String[] args) throws java.lang.Exception
-	{
-		// your code goes here
-		Scanner sc = new Scanner(System.in);
-		    int t = sc.nextInt();
-		    while(t-- > 0){
-		        int n = sc.nextInt();
-		        String A = sc.next();
-		        String B = sc.next();
-		        //we'll be performing only the first operation 
-		        List<int[]> oper = new ArrayList<>();
-		        int k = 0 ; 
-		        int L = -1 ; 
-		        int R = -1 ; 
-		        for(int i = 0 ; i < n; i++){
-		            //traversing over the two arrays simul
-		            if(A.charAt(i) != B.charAt(i)){
-		                L = i ; 
-		                while(i < n && A.charAt(i) != B.charAt(i)) i++ ; 
-		                R = i-1 ; 
-		                oper.add(new int[]{L , R});
-		                k++ ; 
-		            }else{
-		                continue ; 
-		            }
-		        }
-		        System.out.println(k);
-		        for(int[] arr : oper){
-		            System.out.println(1 + " " + (arr[0]+1) + " " + (arr[1]+1));
-		        }
-		    }
-		sc.close();
-	}
+	public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // Scanner sc = new Scanner(System.in);
+        // int t = sc.nextInt();
+        int t = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        while(t-- > 0){
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			
+            int E = n * (n - 1) / 2;
+            int minW = Math.max(0, n - 1 - (E - m));
+            int maxW = Math.min(m, n - 1);
+            if (minW > maxW) {
+				sb.append(0);
+            } else {
+                // Sum of all integers from minW to maxW
+                int count = maxW - minW + 1;
+                int sum = (minW + maxW) * count / 2;
+				sb.append(sum);
+            }
+			sb.append("\n");
+        }
+        System.out.println(sb); 
+    }
 }
