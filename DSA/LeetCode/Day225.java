37ca728d47beb33f9ddb601b8284b39c66a82fbd
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-05-19 09:40:10 +0530

    Day 473

---
+++ DSA/LeetCode/Day225.java
@@ -0,0 +1,26 @@
+package LeetCode;
+public class Day225 {
+    class Solution {
+    public String triangleType(int[] nums) {
+        int a = nums[0];
+        int b = nums[1];
+        int c = nums[2];
+        if( a < b + c && b < c + a && c < b + a){
+            int count = 0 ; 
+            if(a == b)count++ ;
+            if(b == c)count++ ;
+            if(c == a)count++ ;
+            if(count == 3){
+                return "equilateral";
+            }else if(count == 1){
+                return "isosceles";
+            }else{
+                return "scalene";
+            }
+        }else{
+            return "none";
+        }
+        // return "";
+    }
+}
+}
