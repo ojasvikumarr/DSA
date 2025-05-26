250b29530529efa9fe3fdc009c9c3467635dc9d7
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-05-26 19:02:00 +0530

    UNited terms

--- cp86.java
+++ cp86.java
@@ -10,17 +10,35 @@ public class cp86 {
         int t = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
         while(t-- > 0){
+            int n = Integer.parseInt(br.readLine());
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int k = Integer.parseInt(st.nextToken());
-           
-            if(x%k != 0){
-                sb.append(1 + " \n");
-                sb.append(x);
-            }else{
-                sb.append(2 + " \n");
-                sb.append((x-1) + " " + 1 + " ");
+            int[] arr = new int[n];
+            int neg = 0 ; 
+            int pos = 0 ; 
+            for(int i = 0 ; i < n ; i++){
+                arr[i] = Integer.parseInt(st.nextToken());
+                if(arr[i] < 0){
+                    neg++ ; 
+                }else{
+                    pos++ ; 
+                }
             }
+
+            if(pos >= neg){
+                if(neg % 2 == 0){
+                    sb.append(0);
+                }else{
+                    sb.append(1); 
+                }
+            }else{
+                int rem = neg - pos ; 
+                int val = (int)Math.ceil((double)rem/2) ; 
+                neg -= val ; 
+                if(neg % 2 != 0) val++ ; 
+                sb.append(val);
+            }
+           
+            
             
             sb.append("\n");
         }
