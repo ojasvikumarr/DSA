ca3cfea2e90c6f0905522ef534f9acd5fdbf3fbb
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-05-27 15:49:11 +0530

    Grinding coeforces

--- cp87.java
+++ cp87.java
@@ -2,7 +2,7 @@ import java.util.* ;
 import java.io.* ; 
 import java.lang.StringBuilder ; 
 
-public class cp86 {
+public class cp87 {
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         // Scanner sc = new Scanner(System.in);
@@ -13,18 +13,27 @@ public class cp86 {
             int n = Integer.parseInt(br.readLine());
             StringTokenizer st = new StringTokenizer(br.readLine());
             int[] arr = new int[n];
+            int xor = 0 ;
+            boolean zero = false ; 
             for(int i = 0 ;i < n; i++){
                 arr[i] = Integer.parseInt(st.nextToken());
+                xor ^= arr[i];
+                if(arr[i] == 0) zero = true ; 
             }
-            int count = 1; 
-            int prev = arr[0];
-            for(int i = 1 ; i < n ; i++){
-                if(prev + 1 < arr[i]){
-                    count++ ; 
-                    prev = arr[i] ;
+
+            // if((zero && n % 2 == 0 )||(!zero && n%2 != 0)){
+            if(n%2 != 0){
+                //this means its odd 
+                sb.append(xor);
+            }else{
+                //its really even
+                if(xor == 0){
+                    sb.append(0);
+                }else{
+                    sb.append(-1);
                 }
             }
-            sb.append(count);
+            
             sb.append("\n");
         }
         System.out.println(sb.toString());
