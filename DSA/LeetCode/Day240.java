1082e49b058f823917b36830cef9ec9202a26ce0
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-06-03 22:03:30 +0530

    Codeforces Div2 rona agya

---
+++ DSA/LeetCode/Day240.java
@@ -0,0 +1,81 @@
+package LeetCode;
+
+import java.util.HashSet;
+import java.util.Queue;
+import java.util.Set;
+
+public class Day240 {
+    class Solution {
+    public int maxCandies(int[] status, int[] candies, int[][] keys, int[][] containedBoxes, int[] initialBoxes) {
+        Queue<Integer> q = new LinkedList<>();
+        Set<Integer> keyAvailable = new HashSet<>();
+        Set<Integer> boxAvailable = new HashSet<>();
+
+
+        for (int box : initialBoxes) {
+            boxAvailable.add(box);
+            if (status[box] == 1) q.add(box);
+        }
+
+        int result = 0;
+
+        while (!q.isEmpty()) {
+            int box = q.poll();
+            result += candies[box];
+
+            // Add new keys to the keyAvailable set
+            for (int key : keys[box]) {
+                keyAvailable.add(key);
+                if (boxAvailable.contains(key) && status[key] == 0) {
+                    q.add(key);
+                    status[key] = 1; // Unlock the box
+                }
+            }
+
+            // Add contained boxes to the queue or mark them as available
+            for (int containedBox : containedBoxes[box]) {
+                boxAvailable.add(containedBox);
+                if (status[containedBox] == 1 || keyAvailable.contains(containedBox)) {
+                    q.add(containedBox);
+                    status[containedBox] = 1; // Unlock the box
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public int maxCandiesII(int[] status, int[] candies, int[][] keys, int[][] containedBoxes, int[] initialBoxes) {
+        Queue<Integer> q = new LinkedList<>();
+        for(Integer box : initialBoxes ){
+            if(status[box] == 1 ) q.add(box);
+        }
+        Set<Integer> keyAvailable = new HashSet<>();
+        int result = 0 ;
+        boolean[] visited = new boolean[keys.length];
+        boolean[] taken = new boolean[keys.length];
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i = 0 ; i < size ; i++){
+                int src = q.poll();
+                result += candies[src];
+                taken[src] = true ;
+                for(Integer key : keys[src]) keyAvailable.add(key);
+                for(Integer neighbor : containedBoxes[src]){
+                    visited[neighbor] = true ;
+                    if(status[neighbor] == 1 || (status[neighbor] == 0 && keyAvailable.contains(neighbor))){
+                        q.add(neighbor);
+
+                    }
+                }
+            }
+        }
+        for(Integer key : keyAvailable){
+            if(status[key] == 0 && visited[key] == true && taken[key] == false){
+                result += candies[key];
+            }
+        }
+        return result ;
+    }
+}
+}
--- cp96.java
+++ cp96.java
@@ -1,5 +1,6 @@
 import java.io.* ; 
 import java.util.* ; 
+import java.util.Arrays;
 import java.lang.StringBuilder ; 
 
 public class cp96 {
@@ -9,28 +10,46 @@ public class cp96 {
         StringBuilder sb = new StringBuilder();
         int tc = Integer.parseInt(br.readLine());
         while(tc-- > 0){
-            long n = Long.parseLong(br.readLine());
-            int res = 0 ; 
-            while( n%6 == 0 || n%3 == 0){
-                if(n%6 == 0){
-                    n /= 6;
-                }else if(n%3 == 0){
-                    n *= 2;
+            int n = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            long[] arr = new long[n];
+            boolean flag = true ; 
+            for(int i = 0 ; i < n ; i++){
+                arr[i] = Long.parseLong(st.nextToken());
+                if(i > 0){
+                    if(arr[i] != arr[i-1]){
+                        flag = false ;
+                    }
                 }
-                res++;
-                if(n == 1) break ; 
             }
-            if(n == 1){
-                sb.append(res);
-            }else{
-                sb.append(-1);
+            if(flag){
+                sb.append("0");
+                sb.append("\n");
+                continue ;
             }
 
+            int i = 0 ; 
+            long mini = Long.MAX_VALUE;
+
+            while(i < n ){
+                long cost = (long)(i)*arr[i];
+                while(i < n-1 && arr[i] == arr[i+1]){
+                    i++ ; 
+                }
+                cost += (long)(n-i-1)*arr[i];
+                mini = Math.min(mini , cost);
+                // System.out.println("Cost : " + cost + " i : " + i);
+                i++ ; 
+            }
+            // if(i == n-1){
+            //     int cost = (i-1)*arr[i]; 
+            //     mini = Math.min(mini , cost);
+            // }
+            sb.append(mini);
             sb.append("\n");
         }
         System.out.println(sb.toString());
     }
-    // Fast exponentiation for 2^k
     static long pow2(long k) {
         // int MOD = 1000000007;
         long res = 1, base = 2;
