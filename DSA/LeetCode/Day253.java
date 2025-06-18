7d0f7edd5a740f58d5e42cf7d21f14ea534f8f24
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-06-18 18:26:39 +0530

    Perfecct Square

--- DSA/LeetCode/Day253.java
+++ DSA/LeetCode/Day253.java
@@ -17,4 +17,61 @@ public class Day253{
         return (int)dp[n];
     }
 }
+class SolutionII {
+    public int numSquares(int n) {
+        int[] dp = new int[n+1];
+        // dp[i] = minimum no. of perfect sqs req to make a sum of i 
+        Arrays.fill(dp , (int)1e8);
+        dp[0] = 0 ;
+        for(int i = 1 ; i <= n ; i++){
+            //for every i we wil be checking previous perfect sqs AVAILABLE!!
+            for(int prev = 1 ; prev*prev <= i ; prev++){
+                dp[i] = Math.min(dp[i] , 1 + dp[i-prev*prev]);
+            }
+        }
+        return dp[n];
+    }
+    Map<String , Integer> map ;
+    public int solveI(int n){
+        if(n == 0){
+            return 0 ;
+        }
+        if(dp[n] != -1) return dp[n];
+        int res = (int)1e8 ;
+        for(int i = 1 ; i*i <= n ; i++){
+            res = Math.min(res , 1 + solveI(n-i*i));
+        }
+        return dp[n] = res ;
+
+    }
+    int[] dp ;
+    public int solve(int n , int idx){
+        if(n == 0){
+            return 0 ; 
+        }
+        if(idx <= 0){
+            return (int)1e8 ; 
+        }
+
+        String key = n + " " + idx;
+        if(map.containsKey(key)){
+            return map.get(key);
+        }
+        // if(dp[n][idx] != -1){
+        //     return dp[n][idx];
+        // }
+
+        int pick = (int)1e8  ; 
+        if(perf(idx) && n >= idx){
+            pick = 1 + solve( n-idx , idx);
+        }
+        int notpick = 0 + solve(n , idx-1);
+        map.put(key , Math.min(pick , notpick));
+        return map.get(key);
+    }
+    public boolean perf(int x){
+        int sqrt = (int)Math.sqrt(x);
+        return x == sqrt*sqrt ;
+    }
+}
 }
