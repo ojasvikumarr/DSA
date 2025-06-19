19d4208ec19c6687011b6edf25eb689e27363a4d
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-06-19 18:56:23 +0530

    Max PRofit Job

--- cp115.java
+++ cp115.java
@@ -142,4 +142,115 @@ public class cp115{
         return dp[idx][tight][sum] = res%MOD ; 
     }
 }
+class SolutionIv {
+
+
+    public int jobScheduling(int[] start, int[] end, int[] profit) {
+        int n = start.length ;
+
+        int[][] schedule = new int[n][3];
+        for(int i = 0 ; i < n ; i++){
+            schedule[i][0] = start[i];
+            schedule[i][1] = end[i];
+            schedule[i][2] = profit[i];
+        }
+
+        Arrays.sort(schedule , (a , b) -> a[0] - b[0]);
+
+        memo = new int[n+1];
+        Arrays.fill(memo , -1);
+        return solveII(schedule , 0);
+    }
+    int[] memo ; 
+    public int solveII(int[][] sch , int idx){
+        if(idx >= sch.length){
+            return 0 ;
+        }
+        if(memo[idx] != -1){
+            return memo[idx];
+        }
+        int next = bsIdx(sch , idx);
+        int pick = sch[idx][2] + solveII(sch , next);
+        int skip = solveII(sch ,idx+1);
+        return memo[idx] = Math.max(pick , skip);
+    }
+    public int bsIdx(int[][] sch , int idx){
+        int l = idx+1 ; 
+        int r = sch.length-1 ; 
+        int res = 0 ; 
+        while(l <= r){
+            int mid = l + (r-l)/2 ;
+            if(sch[mid][0] >= sch[idx][1]){
+                res = mid ; 
+                r = mid-1 ;
+            }else{
+                l = mid + 1;
+            }
+        }
+        return l ;
+    }
+    public int jobSchedulingII(int[] start, int[] end, int[] profit) {
+        int n = start.length ;
+
+        int[][] schedule = new int[n][3];
+        for(int i = 0 ; i < n ; i++){
+            schedule[i][0] = start[i];
+            schedule[i][1] = end[i];
+            schedule[i][2] = profit[i];
+        }
+
+        Arrays.sort(schedule , (a , b) -> a[0] - b[0]);
+
+        int[] tab = new int[n+1];
+        tab[n-1] = schedule[n-1][2];
+
+        for(int i = n-2 ; i >= 0 ; i--){
+            for(int endIdx = i+1 ; endIdx < n ; endIdx++){
+                if(schedule[endIdx][0] >= schedule[i][1]){
+                    tab[i] = Math.max(schedule[i][2] + tab[endIdx] , tab[i+1]);
+                }
+            }
+            tab[i] = Math.max(schedule[i][2] , tab[i+1]);
+        }
+        return tab[0];
+    }
+
+
+    public int jobSchedulingI(int[] start, int[] end, int[] profit) {
+        int n = start.length ;
+
+        dp = new int[n+1][n+1];
+        for(int[] row : dp) Arrays.fill(row , -1);
+        
+        int[][] schedule = new int[n][3];
+        for(int i = 0 ; i < n ; i++){
+            schedule[i][0] = start[i];
+            schedule[i][1] = end[i];
+            schedule[i][2] = profit[i];
+        }
+
+        Arrays.sort(schedule , (a , b) -> a[0] - b[0]);
+
+        return solve(0 , n , schedule);
+    }
+    int[][] dp ; 
+    public int solve(int idx , int endTimeIdx , int[][] sch){
+        if(idx == sch.length){
+            return 0 ;
+        }
+  
+        if(dp[idx][endTimeIdx] != -1){
+            return dp[idx][endTimeIdx];
+        }
+
+        int pick = 0 ; 
+        if(endTimeIdx == sch.length || sch[endTimeIdx][1] <= sch[idx][0] ){
+            pick = sch[idx][2] + solve(idx+1 , idx , sch);
+        }
+
+        int notpick = 0 + solve(idx + 1, endTimeIdx , sch);
+
+        return dp[idx][endTimeIdx] = Math.max(pick , notpick) ;
+    }
+}
 }
