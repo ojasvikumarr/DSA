86cacbdd4457b3426cc6dd66ef9436908d6510cf
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-06-29 22:32:10 +0530

    Day 514

--- DSA/LeetCode/Day265.java
+++ DSA/LeetCode/Day265.java
@@ -1,29 +1,23 @@
-class Day265 {
-    public int[] maxSubsequence(int[] nums, int k) {
-        int n = nums.length ; 
-        Map<Integer , Integer> map = new HashMap<>();
-        int[] newArr = Arrays.copyOf(nums , n);
-        Arrays.sort(newArr);
-        for(int i = n-1 ; i >= n-k ; i-- ){
-            map.put(newArr[i] , map.getOrDefault(newArr[i] , 0)+1);
+class Solution {
+    public int numSubseq(int[] nums, int target) {
+        int mod = 1000000007 , n = nums.length;
+        Arrays.sort(nums);
+        
+        int[] power = new int[n];
+        power[0] = 1;
+        for (int i = 1; i < n; i++) {
+            power[i] = (power[i - 1] * 2) % mod;
         }
-        int[] res = new int[k];
-        int counter = 0 ; 
-        for(int i = 0 ; i < n ; i++){
-            int ele = nums[i];
-            // System.out.println(ele + " ");
-            if(map.containsKey(ele)){
-                res[counter] = ele ; 
-                int freq = map.get(ele);
-                if(freq == 1){
-                    map.remove(ele);
-                }else{
-                    map.put(ele , freq-1);
-                }
-                counter++ ;
+        int left = 0 , right = n - 1 , result = 0;
+
+        while (left <= right) {
+            if (nums[left] + nums[right] <= target) {
+                result = (result + power[right - left]) % mod;
+                left++;
+            } else {
+                right--;
             }
-            if(counter == k) break ;
         }
-        return res ;
+        return result;
     }
 }
