33a1c86fe140b7b69dafaf986694a0b56aa39953
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-07-01 22:46:52 +0530

    Corerectionsg

--- DSA/LeetCode/Day267.java
+++ DSA/LeetCode/Day267.java
@@ -4,6 +4,7 @@ class Day267 {
         for(int i = 0 ; i < s.length()-1 ; i++){
             if(s.charAt(i) == s.charAt(i+1)) count++ ;
         }
+        //hello
         return count + 1 ;
     }
 }
