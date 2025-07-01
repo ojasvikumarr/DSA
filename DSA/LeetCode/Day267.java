c2c3ad768b5fa8eda7f609e6f9a65fc35ae2929e
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-07-01 22:46:36 +0530

    Day 514

---
+++ DSA/LeetCode/Day267.java
@@ -0,0 +1,9 @@
+class Day267 {
+    public int possibleStringCount(String s) {
+        int count = 0 ;
+        for(int i = 0 ; i < s.length()-1 ; i++){
+            if(s.charAt(i) == s.charAt(i+1)) count++ ;
+        }
+        return count + 1 ;
+    }
+}
