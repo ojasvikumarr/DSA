cb7f6129b17e7dbe94009bd764b245691cc8aebe
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-07-12 23:09:59 +0530

    Day 524

--- Main.java
+++ Main.java
@@ -9,172 +9,117 @@ public class Main {
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int t = Integer.parseInt(br.readLine());
-        while(t-- > 0){
-            int n = Integer.parseInt(br.readLine());
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            long[] arr = new long[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-            Map<Long , Integer> neg = new HashMap<>();
-            Map<Long , Integer> po = new HashMap<>();
-            int negs = 0 ; 
-            int pos = 0 ; 
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-            for(int i = 0 ; i < n ; i++){
-                arr[i] = Long.parseLong(st.nextToken());
-                if(arr[i] < 0){
-                    arr[i] = -arr[i];
-                    neg.put(arr[i] , neg.getOrDefault(arr[i] , 0)+1);
-                    negs++ ; 
-                }else{
-                    po.put(arr[i] , po.getOrDefault(arr[i] , 0)+1);
-                    pos++ ;
-                }
-            }
-
-
-            Arrays.sort(arr);
-            long r = arr[1]/arr[0];
-            boolean flag = true ; 
-            if(n > 2){
-                long num = arr[1];
-                long den = arr[0];
-                for(int i = 1 ; i < n-1 ; i++){
-                    if(arr[i+1]*den != arr[i]*num){
-                        flag = false ; 
-                        break ; 
-                    }
-                }
-            }
-            if(flag == false){
-                sb.append("No");
-                sb.append("\n");
-                continue ;
-            }
-
-            if((pos == n || negs == n)){
-                sb.append("Yes");
-                sb.append("\n");
-                continue ;
-            }else{
-                boolean flagA = true; 
-                Map<Long , Integer> Apos = new HashMap<>(po);
-                Map<Long , Integer> Aneg = new HashMap<>(neg);
-
-                for(int i = 0 ; i < n ; i++){
-                    long a = arr[i];
-                    if(i%2 == 0){
-                        int c = Apos.getOrDefault(a , 0);
-                        if(c == 0){
-                            flagA = false ; 
-                            break ; 
-                        }
-                        Apos.put(a , c-1);
-                    }else{
-                        int c = Aneg.getOrDefault(a , 0);
-                        if(c == 0){
-                            flagA = false ; 
-                            break ; 
-                        }
-                        Aneg.put(a , c-1);
-                    }
-                }
-                boolean flagB = true; 
-                Map<Long , Integer> Bpos = new HashMap<>(po);
-                Map<Long , Integer> Bneg = new HashMap<>(neg);
-
-                for(int i = 0 ; i < n ; i++){
-                    long a = arr[i];
-                    if(i%2 != 0){
-                        int c = Bpos.getOrDefault(a , 0);
-                        if(c == 0){
-                            flagB = false ; 
-                            break ; 
-                        }
-                        Bpos.put(a , c-1);
-                    }else{
-                        int c = Bneg.getOrDefault(a , 0);
-                        if(c == 0){
-                            flagB = false ; 
-                            break ; 
-                        }
-                        Bneg.put(a , c-1);
-                    }
-                }
-
-                if(flagA || flagB){
-                    sb.append("Yes");
-                }else{
-                    sb.append("No");
-                }
-                sb.append("\n");
-                continue ;
-            }
-            // if(neg.size() > 0 && flag ){
-            //     //now assign neg values 
-            //     if(neg.size() == 1){
-            //         //that is toggling is happening 
-            //         if(n%2 == 0){
-            //             if(neg.get(arr[0]) != n/2){
-            //                 flag = false ;
-            //             }
-            //         }else{
-            //             //it can be n/2 or n/2 + 1
-            //             if(neg.get(arr[0]) == n/2 || neg.get(arr[0]) == n/2 + 1){
-            //                 flag = true ;
-            //             }else{
-            //                 flag = false ;
-            //             }
-            //         }
-            //     }else{
-            //         //toggle 
-            //         if(neg.containsKey(arr[0])){
-            //             for(int i = 0 ; i < n ; i += 2){
-            //                 if(neg.get(arr[i]) == 0){
-            //                     flag = false ; 
-            //                     break ; 
-            //                 }else{
-            //                     neg.put(arr[i] , neg.get(arr[i]) - 1);
-            //                 }
-            //             }
-            //             for(int key : neg.keySet()){
-            //                 if(neg.get(key) > 0){
-            //                     flag = false ; 
-            //                     break ;
-            //                 }
-            //             }
-            //         }else{
-            //             for(int i = 1 ; i < n ; i += 2){
-            //                 if(neg.get(arr[i]) == 0){
-            //                     flag = false ; 
-            //                     break ; 
-            //                 }else{
-            //                     neg.put(arr[i] , neg.get(arr[i]) - 1);
-            //                 }
-            //             }
-            //             for(int key : neg.keySet()){
-            //                 if(neg.get(key) > 0){
-            //                     flag = false ; 
-            //                     break ;
-            //                 }
-            //             }
-            //         }
-            //     }
-            // }
-           
-
-            // if(flag){
-            //     sb.append("Yes");
-            // }else{
-            //     sb.append("No");
-            // }
-            // sb.append("\n");
+        st = new StringTokenizer(br.readLine());
+        long[] arr = new long[n];
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = Long.parseLong(st.nextToken());
         }
-        System.out.println(sb.toString());
 
+        if(m == n){
+            System.out.println(0);
+            return ; 
+        }
+
+        Arrays.sort(arr);
+
+        long[] gaps = new long[n-1];
+
+        for(int i = 0 ; i < n ; i++){
+            gaps[i] = arr[i+1] - arr[i];
+        }
+
+        init(n);
+
+
+        
         br.close();
     }
+    static int[] segTree; 
+    public static void init(int n ,long[] gaps){
+        segTree = new int[4*n] ; 
+        buildTree();
+    }
+    public static void buildTree(int i , int l , int r , long[] gaps){
+        if(l == r){
+            segTree[i] = l ; 
+            return ; 
+        }
+        int mid = l + (r - l)/2 ; 
+        buildTree(2*i + 1 , l , mid , gaps);
+        buildTree(2*i + 2 , mid +1 , r , gaps);
+        int left = segTree[2*i + 1];
+        int right = segTree[2*i + 2];
+        if(gaps[left] >= gaps[right]){
+            segTree[i] = left ;
+        }else{
+            segTree[i] = right ;
+        }
+    }
+
+    public static int query(int i , int l , int r , int start , int end , long[] gaps){
+        if(l > end || r < start) return -1 ; 
+        if(l >= start && r <= end){
+            return segTree[i];
+        }
+
+        int mid = l + (r - l)/2 ; 
+        int left = query(2*i +1 , l , mid , start , end , gaps);
+        int right = query(2*i +2 , mid  + 1 , r , start , end , gaps);
+        if(left == -1){
+            return right ; 
+        }else if(right == -1){
+            return left ;
+        }
+        if(gaps[left] >= gaps[right]) return left ; 
+        return right ;
+    }
+
+    public static long[] bestSplit(int l , int r , long[] arr , long[] gaps , int gN){
+        int n = arr.length ; 
+        long orginal = cost(arr[l] , arr[r]);
+        int k = query(0 , 0 , n-1 , l , r-1 , gaps);
+
+        long leftCost = cost(arr[l] , arr[k]);
+        long rightCost = cost(arr[k+1] ,arr[r] );
+        long saving = orginal - leftCost - rightCost ; 
+        return new Segment(l , r , k , saving);
+    }
+
+    public static long ocst(long l , long r){
+        long span = r - l ; 
+        if(span <= 2) return 0 ; 
+        long need = (span + 1)/2 ; 
+        int x = 0 ; 
+        long power = 1 ; 
+        while(power < need){
+            power = power<<1 ; 
+            x++ ; 
+        }
+        return x ; 
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
