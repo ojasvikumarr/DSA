9ae9e611a6b8acd7e1cb3207cb742d60d5c98a70
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-07-15 23:01:51 +0530

    Day 530

---
+++ DSA/LeetCode/Day287.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isValid(String word) {
+        Set<Character> set = new HashSet<>(Arrays.asList('a' , 'e' , 'i' , 'o' , 'u' , 
+                                                         'A' , 'E' , 'I' , 'O' , 'U'));
+
+        if(word.length() < 3){
+            return false ;
+        }
+
+        boolean vowel = false ; 
+        boolean consonant = false ;
+        for(char c : word.toCharArray()){
+            if(set.contains(c)){
+                vowel = true ;
+            }else if( c >= 'a' && c <= 'z' ||
+                c >= 'A' && c <= 'Z'){
+                consonant = true ;
+            }
+            if( c >= 'a' && c <= 'z' ||
+                c >= 'A' && c <= 'Z' || 
+                c >= '0' && c <= '9'){
+                continue ;
+            }else{
+                return false ;
+            }
+        }
+        if(vowel && consonant){
+            return true ;
+        }else{
+            return false ;
+        }
+    }
+}
