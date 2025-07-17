e89415981db865a132aad00d65431f42a1aa430a
Ojasvi Kumar <ojasvikumar007@gmail.com>
2025-07-17 23:13:15 +0530

    BottomUp

--- DSA/LeetCode/Day289.java
+++ DSA/LeetCode/Day289.java
@@ -116,4 +116,26 @@ class Solution {
         
     //     return dp[idx][prev] = Math.max(pick , skip);
     // }
+
+    public int maximumLength(int[] nums, int k) {
+        return solve(nums , k);
+    }
+    public int solve(int[] nums , int k){
+        int n = nums.length ; 
+        int[][] dp = new int[n][k+1];
+
+        for(int[] row : dp) Arrays.fill(row , 1);
+
+        int maxi = 1 ; 
+
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < i ; j++){
+                int mod = (nums[i] + nums[j])%k ; 
+                dp[i][mod] = Math.max(dp[i][mod] , 1 + dp[j][mod]);
+                maxi = Math.max(maxi , dp[i][mod]) ;
+            }
+        }
+        return maxi ;
+    }
+
 }
